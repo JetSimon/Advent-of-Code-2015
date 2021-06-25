277cc0b98a311bd1005bfab59990eb86ce34d9b7
Jet Simon <jetjdsimon@gmail.com>
2021-06-25 07:31:55 -0700

    8

--- Solutions/Day 8/day8.py
+++ Solutions/Day 8/day8.py
@@ -1,30 +1,12 @@
-commands = []
+score = 0 
 
-inputFile = open('input.txt', 'r') 
+f = open('input.txt', 'r')
+for line in f:
+    raw = line.strip()
+    l = len(raw) + 2
+    l += raw.count('"')
+    l += raw.count("\\")
+    score += l - len(raw)
+print(score)
 
-for line in inputFile:
-    stripped = line.rstrip()
-    commands.append(stripped)
-inputFile.close()
-
-def solve(commands):
-    totalLit = 0
-    totalParse = 0
-    for command in commands:
-        passHex = False
-        literalLength = len(command)
-        parsedLength = literalLength - 2
-        for i in range(1,len(command),1):
-            if(passHex):
-                passHex = False
-            chunk = command[i:i+2]
-            if(len(chunk) == 2 and not passHex):
-                if(chunk[0] == "\\" and chunk[1] != "x"):
-                    parsedLength-=1
-                elif(chunk[0] == "\\" and chunk[1] == "x"):
-                    parsedLength-=3
-                    passHex = True
-        totalLit = literalLength
-        totalParse = totalParse
-    return totalLit - totalParse
-print(solve(commands))
+    
